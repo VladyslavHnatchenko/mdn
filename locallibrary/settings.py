7ba01b039973c9615bbdf2fe826564de7307c102
vladyslav.hnatchenko <hnatchenko.vladyslav@gmail.com>
2019-02-14 15:10:12 +0200

    2/14/19

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -28,7 +28,7 @@ DEBUG = True
 # DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 # DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
-ALLOWED_HOSTS = ['127.0.0.1', 'hnatchenkovladyslav.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'hnatchenkovladyslav.pythonanywhere.com', 'testserver']
 
 
 # Application definition
